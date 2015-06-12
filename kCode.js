0154feab3c73e160a0d6c065b783c8ade2e68fe3
Miguel Costa <maiquedi@gmail.com>
2015-06-12 07:28:16 +0100

    kCode version 1.0
    does not depend on jquery anymore

--- kCode.js
+++ kCode.js
@@ -1,65 +1,91 @@
-(function() {
+(function(window){
 
-    // change this to whichever function you wanna run when the code is successful
-    // TODO: maybe write random words
-    var callback = function(){
-      alert("You bitch...");
-    }
 
-    var kCode = ["Up", "Up", "Down", "Down", "Left", "Right", "Left", "Right", "U+0042", "U+0041"];
-    var input = [];
-    var alertTimerId = undefined;
-    var time = 5000;
 
-    function clear(input) {
-        console.log("clearing...");
-        while (input.length > 0) {
-            input.pop();
-        }
-    }
+    function konamiCode(options, callback, params) {
+      // TODO: code option must have at least 2 characters
 
-    function compare(input, code) {
-        /* check if input is at least, going the right way */
-        var result = false;
 
-        if (input.length <= code.length) {
-            result = true;
-            for (var i = 0; i < input.length; i++) {
-                if (input[i] !== code[i]) {
-                    result = false;
-                }
-            }
-        }
-        if (result === false) {
-            clear(input);
-        }
+      var options = options || {};
 
-        return result;
-    }
+      function disableKonamiCode() {
+          window.document.removeEventListener('keyup', konamiCodePress, false);
+          pressedKeys = [];
+       }
 
-    document.addEventListener('keyup', function(event) {
-        input.push(event.keyIdentifier);
-        console.log("input:", input);
-        if (compare(input, kCode)) {
-            // please note that compare will return true if it matches the beginning of the code, even if it is not complete!
-            if (input.length === kCode.length) {
-                if (typeof("callback") === "undefined"){
-                  console.log("enabled!");
-                }
-                else{
-                  callback();
-                }
-                clearTimeout(alertTimerId);
-                clear(input);
-            } else {
-                console.log("go on...");
-            }
-        } else {
-            if (typeof(alertTimerId) === "undefined") {
-                clearTimeout(alertTimerId);
-            }
-            alertTimerId = setTimeout(clear(input), time);
-        }
-    });
 
-}());
+      var TIMER = 1000 || options.timer, //1s
+          COMBINATION=[38,38,40,40,37, 39,37,39,66,65] || options.combination, //e.which, the keycode
+          DEBUG = false || options.debug,
+          CALLBACK = callback || function defaultCallback(){alert("porn");},
+          PARAMS = params || {},
+          DISABLE = options.disable || false;
+
+      var interval,
+          pressedKeys = [],
+          konamiCodePress = function konamiCodePressEvent(e){
+
+          if (interval) {
+             // clear timeout on key pressed
+             clearTimeout(interval);
+          }
+
+          var curPos = pressedKeys.length,
+              pressedKeyCode = e.which;
+
+         if (DEBUG) { console.log("You pressed",pressedKeyCode); }
+
+          if (pressedKeyCode === COMBINATION[curPos]) {
+              pressedKeys.push(pressedKeyCode);
+              if (DEBUG) { console.log(pressedKeys); }
+
+              if (curPos + 1 === COMBINATION.length) {
+                 // then it was enabled
+                 if (DEBUG) { console.log("enabled!") }
+                  CALLBACK(PARAMS);
+
+                  // reset pressed keys
+                  pressedKeys = [];
+
+              }
+              else {
+                  // right key, but code still incomplete
+                  interval = setTimeout(function(){ pressedKeys = [];}, TIMER)
+              }
+          }
+          else {
+            // wrong key, biatch!
+            if (DEBUG) { console.log("wrong key, reset!\nBut is it the first one?");}
+
+            // still, you may be on to something if, for example, the 3rd character pressed is actually the same as the second right one... you just need to ignore the first pressed key
+            // that will be done... later!
+            pressedKeys = [];
+            // if (pressedKeyCode === COMBINATION[0]) {
+            //       pressedKeys.push(pressedKeyCode);
+            //       interval = setTimeout(function(){ pressedKeys = [];}, TIMER);
+            // }
+          }
+      };
+
+
+      if (DEBUG){
+          console.log(TIMER, COMBINATION, DEBUG, CALLBACK, PARAMS);
+      }
+
+      if (DISABLE){
+          disableKonamiCode();
+          return;
+      }
+
+      window.document.addEventListener("keyup", konamiCodePress, false);
+
+
+}
+
+// link to window element
+if (window.konamiCode) {
+    window.konamiCode({disable:true})
+}
+window.konamiCode = konamiCode;
+
+})(window);
